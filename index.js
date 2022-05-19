7e74004c889f7af99405dbd186a0ce33d687e885
James Parrish <jthomasparrishjr@gmail.com>
2022-05-18 22:05:12 -0500

    commit tree 75693404778ef86ae38e19002e5d57b0efed956f

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 1000) + 1;
+let num3 =16
+let num4 = 6
+const mod = num3 % num4;
+const max = Math.max(1, 2, 3, 4, 20);
